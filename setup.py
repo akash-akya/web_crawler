2a0c2b2c4294b24101209e07e84209c1406a980d
Akash Hiremath <akash.h@teewe.in>
2018-04-06 15:32:52 +0530

    Added README.md and modified setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='web_crawler',
-      version='0.01',
+      version='0.1',
       description='Simple Web Crawler ',
       author='Akash Hiremath',
       author_email='akashh246@gmail.com',
-      packages=['web_crawler'])
+      install_requires=[
+          'requests',
+          'BeautifulSoup'
+      ],
+      packages=find_packages())
